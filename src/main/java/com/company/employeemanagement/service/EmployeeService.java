92536bcba893ee21e8429ab5366e6fd3df98fc38
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-07-19 03:07:19 +0530

    feat: add employee service

---
+++ src/main/java/com/company/employeemanagement/service/EmployeeService.java
@@ -0,0 +1,32 @@
+package com.company.employeemanagement.service;
+
+import com.company.employeemanagement.entity.Employee;
+import com.company.employeemanagement.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
