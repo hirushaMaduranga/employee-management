ac86cc3d3e4270aacdeb980ae989e46c32afa66d
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-07-19 03:57:55 +0530

    feat: add employee search and status filtering

--- src/main/java/com/company/employeemanagement/repository/EmployeeRepository.java
+++ src/main/java/com/company/employeemanagement/repository/EmployeeRepository.java
@@ -2,8 +2,11 @@ package com.company.employeemanagement.repository;
 
 import com.company.employeemanagement.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository
+        extends JpaRepository<Employee, Long>,
+        JpaSpecificationExecutor<Employee> {
 
     long countByStatusIgnoreCase(String status);
 }
