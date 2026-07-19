206022bad0666a828ef90e67610b1615fa2102fe
Hirusha Maduranga <hirushapathum2003@gmail.com>
2026-07-19 08:52:24 +0530

    feat: add employee uniqueness queries

--- src/main/java/com/company/employeemanagement/repository/EmployeeRepository.java
+++ src/main/java/com/company/employeemanagement/repository/EmployeeRepository.java
@@ -9,4 +9,25 @@ public interface EmployeeRepository
         JpaSpecificationExecutor<Employee> {
 
     long countByStatusIgnoreCase(String status);
+
+    boolean existsByEmployeeCode(String employeeCode);
+
+    boolean existsByNic(String nic);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByEmployeeCodeAndEmployeeIdNot(
+            String employeeCode,
+            Long employeeId
+    );
+
+    boolean existsByNicAndEmployeeIdNot(
+            String nic,
+            Long employeeId
+    );
+
+    boolean existsByEmailAndEmployeeIdNot(
+            String email,
+            Long employeeId
+    );
 }
